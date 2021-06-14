a3a8c9558a843c649e7adb00f3002a9001745d50
+ <+>
2021-06-13 18:27:52 -0700

    commit tree fac67fdd8945f559909a904e5e258b454ad2ff54

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+// multiply the variables num1 and num2; 
+// the result of the multiplication should be 62.
+var num1 = 2;
+var num2 = 31;
+var multiply = num1 * num2;
+
+// Generate a random integer greater than 0.
+var random = Math.floor(Math.random() * 0) + 1;
+
+// Create a variable called mod set to an equation that will 
+// calculate the remainder of dividing variable num3 by num4; 
+// the remainder should be 4.
+var num3 = 14;
+var num4 = 5;
+var mod = num3 % num4;
+
+// Create a variable called max that finds the highest number in a set; 
+// the value returned should be 20.
+var max = Math.max(1,3,5,6,20);
